f165279a930255454912eeb82a7a949a93d0deed
Christian Albertini <cja12classic@hotmail.com>
2024-06-10 18:52:06 -0400

    added comments

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -18,14 +18,14 @@ engine=create_engine("sqlite:///Resources/hawaii.sqlite")
 # reflect an existing database into a new model
 Base=automap_base()
 # reflect the tables
-Base.prepare(engine)
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
 measurement=Base.classes.measurement
 station=Base.classes.station
 
 # Create our session (link) from Python to the DB
-session=Session(engine)
+# session=Session(engine)
 
 #################################################
 # Flask Setup
@@ -35,6 +35,7 @@ app=Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+#Welcome page
 @app.route("/")
 def welcome():
     """List all available API routes."""
@@ -51,9 +52,12 @@ def welcome():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     """List all precipitation data"""
+    session=Session(engine)
+    #Get latest day and calculate 1 year earlier from that
     latest=session.query(measurement.date).order_by(measurement.date.desc()).first()
     latest=dt.datetime.strptime(latest[0], '%Y-%m-%d')
     one_year=dt.date(latest.year -1,latest.month, latest.day)
+    #Query for dates and precipitation for one year
     data = session.query(measurement.date, measurement.prcp).\
         filter(measurement.date >= one_year).\
         all()
@@ -72,6 +76,8 @@ def precipitation():
 @app.route("/api/v1.0/stations")
 def stations():
     """List all Stations"""
+    session=Session(engine)
+    #Query for all stations
     data=session.query(station.station).order_by(station.station).all()
 
     session.close()
@@ -88,9 +94,12 @@ def stations():
 @app.route("/api/v1.0/tobs")
 def tobs():
     """List temperatures from last year"""
+    session=Session(engine)
+    #Get latest day and calculate 1 year earlier from that
     latest=session.query(measurement.date).order_by(measurement.date.desc()).first()
     latest=dt.datetime.strptime(latest[0], '%Y-%m-%d')
     one_year=dt.date(latest.year -1,latest.month, latest.day)
+    #Query for date and temperatures in the last year
     data=session.query(measurement.date, measurement.tobs).filter(measurement.date>=one_year).all()
     session.close()
 
@@ -107,9 +116,11 @@ def tobs():
 @app.route("/api/v1.0/<start>")
 def find_start(start_date):
     """Find temperatures starting at certain date"""
+    session=Session(engine)
+    #Query to find the min, max and average temperature from start date onwards
     data=session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).\
     filter(measurement.date>=start_date).all()
-    session.cloe()
+    session.close()
 
     #create dict from list
     tobs_data=[]
@@ -124,10 +135,12 @@ def find_start(start_date):
 
 @app.route("/api/v1.0/<start>/<end>")
 def find_start_end(start_date, end_date):
+    #Query to find the min, max, and average temperatures from one date to another
     """Find temperatures between two set dates"""
+    session=Session(engine)
     data=session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).\
     filter(measurement.date>=start_date).filter(measurement.date<=end_date).all()
-    session.cloe()
+    session.close()
 
     #create dict from list
     tobs_data=[]
@@ -140,6 +153,6 @@ def find_start_end(start_date, end_date):
 
     return jsonify(tobs_data)
 
-
+#stop running webpage
 if __name__ == '__main__':
     app.run(debug=True)
