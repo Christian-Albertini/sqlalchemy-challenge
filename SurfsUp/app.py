b84fb368e37ebc68a21bd45b4bd27453927c37ba
Christian Albertini <cja12classic@hotmail.com>
2024-06-10 19:54:41 -0400

    updating code

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -25,7 +25,7 @@ measurement=Base.classes.measurement
 station=Base.classes.station
 
 # Create our session (link) from Python to the DB
-# session=Session(engine)
+session=Session(engine)
 
 #################################################
 # Flask Setup
@@ -52,7 +52,7 @@ def welcome():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     """List all precipitation data"""
-    session=Session(engine)
+    #session=Session(engine)
     #Get latest day and calculate 1 year earlier from that
     latest=session.query(measurement.date).order_by(measurement.date.desc()).first()
     latest=dt.datetime.strptime(latest[0], '%Y-%m-%d')
@@ -76,7 +76,7 @@ def precipitation():
 @app.route("/api/v1.0/stations")
 def stations():
     """List all Stations"""
-    session=Session(engine)
+    #session=Session(engine)
     #Query for all stations
     data=session.query(station.station).order_by(station.station).all()
 
@@ -94,7 +94,7 @@ def stations():
 @app.route("/api/v1.0/tobs")
 def tobs():
     """List temperatures from last year"""
-    session=Session(engine)
+    #session=Session(engine)
     #Get latest day and calculate 1 year earlier from that
     latest=session.query(measurement.date).order_by(measurement.date.desc()).first()
     latest=dt.datetime.strptime(latest[0], '%Y-%m-%d')
@@ -116,7 +116,7 @@ def tobs():
 @app.route("/api/v1.0/<start>")
 def find_start(start_date):
     """Find temperatures starting at certain date"""
-    session=Session(engine)
+    #session=Session(engine)
     #Query to find the min, max and average temperature from start date onwards
     data=session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).\
     filter(measurement.date>=start_date).all()
@@ -137,7 +137,7 @@ def find_start(start_date):
 def find_start_end(start_date, end_date):
     #Query to find the min, max, and average temperatures from one date to another
     """Find temperatures between two set dates"""
-    session=Session(engine)
+    #session=Session(engine)
     data=session.query(func.min(measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).\
     filter(measurement.date>=start_date).filter(measurement.date<=end_date).all()
     session.close()
